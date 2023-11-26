f6f5c1d904ab8c8cfaa8f0bbaa2915879522bf09
Andreas Lildballe <s214387@dtu.dk>
2023-11-26 16:30:14 +0100

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,19 +2,26 @@ import chisel3.emitVerilog
 import communication.chisel.lib.uart.{UartTx}
 import systolic_array.SystolicArray
 
-/*
-object Main extends App {
-  val inputsL1: Array[Array[Float]] = Array(Array(1.0f, 2.3f, 3.0f), Array(4.0f, 5.05f, 6.0f), Array(7.0f, 8.6f, 9.0f))
-  val weightsL1: Array[Array[Float]] = Array(Array(1.0f, 2.3f, 3.0f), Array(4.0f, 5.05f, 6.0f), Array(7.0f, 8.6f, 9.0f))
-  val biasesL1: Array[Array[Float]] = Array(Array(1.0f, 2.3f, 3.0f), Array(4.0f, 5.05f, 6.0f), Array(7.0f, 8.6f, 9.0f))
-  val signL1: Int = 0
-  val fixedPointL1: Int = 6
 
-  val inputsL2: Array[Array[Float]] = Array(Array(0f, 0f, 0f), Array(4.0f, 5.05f, 6.0f), Array(7.0f, 8.6f, 9.0f))
-  val weightsL2: Array[Array[Float]] = Array(Array(0f, 0f, 0f), Array(4.0f, 5.05f, 6.0f), Array(7.0f, 8.6f, 9.0f))
-  val biasesL2: Array[Array[Float]] = Array(Array(0f, 0f, 0f), Array(4.0f, 5.05f, 6.0f), Array(7.0f, 8.6f, 9.0f))
+object Main extends App {
+  val frequency = 50000000 * 2
+  val baudRate = 115200
+  val w = 8
+
+  val dimension = 3
+
+  val inputsL1: Array[Array[Float]] = Array(Array(1.2f, 1.3f, 2.4f), Array(0.9f, 3.4f, 0.9f), Array(2.2f, 1.2f, 0.9f))
+  val weightsL1: Array[Array[Float]] = Array(Array(2.2f, 1.3f, 1.0f), Array(4.9f, 0.4f, 4.8f), Array(2.2f, 1.2f, 0.9f))
+  val biasesL1: Array[Array[Float]] = Array(Array(1.0f, 1.0f, 1.0f), Array(1.0f, 1.0f, 1.0f), Array(1.0f, 1.0f, 1.0f))
+  val signL1: Int = 0
+  val fixedPointL1: Int = 1
+
+  val inputsL2: Array[Array[Float]] = Array(Array(0.0f, 0.0f, 0.0f), Array(0.0f, 0.0f, 0.0f), Array(0.0f, 0.0f, 0.0f))
+  val weightsL2: Array[Array[Float]] = Array(Array(1.0f, 0.9f, 0.8f), Array(0.7f, 0.6f, 0.4f), Array(0.3f, 0.2f, 0.1f))
+  val biasesL2: Array[Array[Float]] = Array(Array(0.0f, 0.0f, 0.0f), Array(1.0f, 1.0f, 1.0f), Array(1.0f, 1.0f, 1.0f))
   val signL2: Int = 0
-  val fixedPointL2: Int = 4
+  val fixedPointL2: Int = 1
+
 
   val inputs: Array[Array[Array[Int]]] = Array(
     Configuration.convertFloatMatrixToFixedMatrix(inputsL1, fixedPointL1),
@@ -36,16 +43,15 @@ object Main extends App {
   var mappedWeights = Configuration.mapWeights(weights)
   var mappedBiases = Configuration.mapBiases(biases)
 
-  emitVerilog(new Accelerator(w = 16, dimension = 4,
-    mappedInputs, mappedWeights,
-    mappedBiases, signs, fixedPoints))
+
+  emitVerilog(new IdealAccelerator(w, dimension, frequency, baudRate, mappedInputs, mappedWeights, mappedBiases, signs, fixedPoints))
 }
 
-*/
 
+/*
 
 object Main extends App {
   print("UART \n")
   emitVerilog(new UartTx(50000000 * 2, 115200))
 }
-
+*/
