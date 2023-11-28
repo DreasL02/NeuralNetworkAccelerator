a890782b5e9376b9e0508ff25e69131c60a10195
Andreas Lildballe <s214387@dtu.dk>
2023-11-28 22:01:57 +0100

    Comments in systolic array

--- src/main/scala/systolic_array/SystolicArray.scala
+++ src/main/scala/systolic_array/SystolicArray.scala
@@ -10,34 +10,38 @@ class SystolicArray(w: Int = 8, dimension: Int = 4) extends Module {
     val c = Output(Vec(dimension, Vec(dimension, UInt(w.W))))
 
     val fixedPoint = Input(UInt(log2Ceil(w).W))
-    val clear = Input(Bool())
+    val clear = Input(Bool()) // clears all registers in the PEs
   })
 
-  val processingElements = VecInit.fill(dimension, dimension)(Module(new ProcessingElement(w)).io)
+  // Inspired by:
+  // https://github.com/kazutomo/Chisel-MatMul/tree/master
+
+  val processingElements = VecInit.fill(dimension, dimension)(Module(new ProcessingElement(w)))
 
   for (column <- 0 until dimension) {
     for (row <- 0 until dimension) {
       //Vertical inputs
       if (column == 0) {
         //Take from buffer
-        processingElements(0)(row).aIn := io.a(row)
+        processingElements(0)(row).io.aIn := io.a(row)
       } else {
         //Take from previous PE
-        processingElements(column)(row).aIn := processingElements(column - 1)(row).aOut
+        processingElements(column)(row).io.aIn := processingElements(column - 1)(row).io.aOut
       }
       //Horizontal inputs
       if (row == 0) {
         //Take from buffer
-        processingElements(column)(0).bIn := io.b(column)
+        processingElements(column)(0).io.bIn := io.b(column)
       } else {
         //Take from previous PE
-        processingElements(column)(row).bIn := processingElements(column)(row - 1).bOut
+        processingElements(column)(row).io.bIn := processingElements(column)(row - 1).io.bOut
       }
 
-      io.c(column)(row) := processingElements(column)(row).cOut
-
-      processingElements(column)(row).fixedPoint := io.fixedPoint
-      processingElements(column)(row).clear := io.clear
+      // map outputs
+      io.c(column)(row) := processingElements(column)(row).io.cOut
+      
+      processingElements(column)(row).io.fixedPoint := io.fixedPoint
+      processingElements(column)(row).io.clear := io.clear
     }
   }
 }
