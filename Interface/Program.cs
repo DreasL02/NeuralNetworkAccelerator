1e1d1ac4729158971ade6d1d85cee099b256ba5d
Andreas Lildballe <s214387@dtu.dk>
2023-11-28 21:47:14 +0100

    Added Interface to main repo

---
+++ Interface/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO.Ports;
+
+class Program
+{
+	static public void Main(string[] args)
+	{
+		var nna = new NeuralNetworkAcceleratorInterface("COM6", 115200, 13, 3, 25);
+        
+        nna.Transmit();
+        nna.Calculate();
+        nna.Transmit();
+        nna.Address();
+        nna.Address();
+        nna.Transmit();
+        nna.Send([1, 0, 23, 10, 12, 
+                  0, 0, 1, 0, 2, 
+                  1, 2, 4, 2, 1,
+                  2, 12, 1
+            ]);
+        nna.Transmit();
+        nna.Address();
+        nna.Transmit();
+        nna.Calculate();
+        nna.Calculate();
+        nna.Transmit();
+    }
+}
