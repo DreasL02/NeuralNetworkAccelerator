0fe50a8f4b90c1eaab25a62ad9d9469b039f0d55
Andreas Lildballe <s214387@dtu.dk>
2023-11-28 22:29:05 +0100

    comments

--- src/main/scala/ByteSplitter.scala
+++ src/main/scala/ByteSplitter.scala
@@ -1,6 +1,7 @@
 import chisel3._
 import chisel3.util.Cat
 
+// Scalable byte splitter of a w bit input into a Vec of 8 bit outputs
 class ByteSplitter(w: Int = 8) extends Module {
   private val numberOfBytes = (w.toFloat / 8.0f).ceil.toInt
 
@@ -9,19 +10,22 @@ class ByteSplitter(w: Int = 8) extends Module {
     val output = Output(Vec(numberOfBytes, UInt(8.W)))
   })
   // splits in order: (0) is lsb, (numberOfBytes - 1) is msb
-  var bitsNotGoneThrough = w
+  var bitsNotGoneThrough = w  // number of bits that has not gone through the splitter
   for (i <- 0 until numberOfBytes) {
     val byte = WireInit(0.U(8.W))
-    if (bitsNotGoneThrough < 8) {
-      byte := io.input(i * 8 + (bitsNotGoneThrough - 1), i * 8)
-    } else {
-      byte := io.input(i * 8 + 7, i * 8)
+
+    if (bitsNotGoneThrough < 8) { // if there are less than 8 bits left
+      byte := io.input(i * 8 + (bitsNotGoneThrough - 1), i * 8) // take the remaining bits
+    } else { // if there are 8 or more bits left
+      byte := io.input(i * 8 + 7, i * 8) // take the next 8 bits
     }
-    io.output(i) := byte
-    bitsNotGoneThrough = bitsNotGoneThrough - 8
+
+    io.output(i) := byte // output the byte
+    bitsNotGoneThrough = bitsNotGoneThrough - 8 // decrement the number of bits left
   }
 }
 
+// Scalable byte splitter of a Vec of w bit inputs into a Vec of 8 bit outputs
 class VectorIntoByteSplitter(w: Int, dimension: Int) extends Module {
   private val numberOfBytes = (w.toFloat / 8.0f).ceil.toInt
 
@@ -30,15 +34,16 @@ class VectorIntoByteSplitter(w: Int, dimension: Int) extends Module {
     val output = Output(Vec(dimension * dimension * numberOfBytes, UInt(8.W)))
   })
 
-  for (i <- 0 until dimension * dimension) {
+  for (i <- 0 until dimension * dimension) { // for each vector element
     val byteSplitter = Module(new ByteSplitter(w))
-    byteSplitter.io.input := io.input(i)
-    for (j <- 0 until numberOfBytes) {
-      io.output(i * numberOfBytes + j) := byteSplitter.io.output(j)
+    byteSplitter.io.input := io.input(i) // split the vector element into bytes
+    for (j <- 0 until numberOfBytes) { // for each byte
+      io.output(i * numberOfBytes + j) := byteSplitter.io.output(j) // output the byte
     }
   }
 }
 
+// Scalable byte collector of a Vec of 8 bit inputs into a w bit output
 class ByteCollector(w: Int = 8) extends Module {
   private val numberOfBytes = (w.toFloat / 8.0f).ceil.toInt
   val io = IO(new Bundle {
@@ -47,9 +52,10 @@ class ByteCollector(w: Int = 8) extends Module {
     val output = Output(UInt(w.W))
   })
 
-  io.output := Cat(io.input.reverse)
+  io.output := Cat(io.input.reverse) // concatenates the bytes in order specified above
 }
 
+// Scalable byte collector of a Vec of 8 bit inputs into a Vec of w bit outputs
 class ByteIntoVectorCollector(w: Int, dimension: Int) extends Module {
   private val numberOfBytes = (w.toFloat / 8.0f).ceil.toInt
 
@@ -58,11 +64,11 @@ class ByteIntoVectorCollector(w: Int, dimension: Int) extends Module {
     val output = Output(Vec(dimension * dimension, UInt(w.W)))
   })
 
-  for (i <- 0 until dimension * dimension) {
+  for (i <- 0 until dimension * dimension) { // for each vector element
     val byteCollector = Module(new ByteCollector(w))
-    for (j <- 0 until numberOfBytes) {
-      byteCollector.io.input(j) := io.input(i * numberOfBytes + j)
+    for (j <- 0 until numberOfBytes) { // for each byte
+      byteCollector.io.input(j) := io.input(i * numberOfBytes + j) // collect the bytes into a vector element
     }
-    io.output(i) := byteCollector.io.output
+    io.output(i) := byteCollector.io.output // output the collected vector element
   }
 }
--- src/main/scala/Rectifier.scala
+++ src/main/scala/Rectifier.scala
@@ -8,11 +8,11 @@ class Rectifier(w : Int = 8, dimension : Int = 4) extends Module{
 
   for (column <- 0 until dimension) {
     for (row <- 0 until dimension) {
-      io.result(column)(row) := io.values(column)(row)
+      io.result(column)(row) := io.values(column)(row) //default is the same value
 
       when(io.signed){ //if the values are signed
-        when(io.values(column)(row) >> (w-1).U === 1.U) { //if signed bit is 1, the result is negative
-          io.result(column)(row) := 0.U
+        when(io.values(column)(row) >> (w-1).U === 1.U) { //if signed bit (@msb) is 1, the result is negative
+          io.result(column)(row) := 0.U //ReLU gives 0
         }
       }
     }
