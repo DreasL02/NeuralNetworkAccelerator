8e33c6ade0503b052efc131b9033306142e9081f
Ivan Hansgaard Hansen <ivanhhansen@gmail.com>
2024-02-08 12:09:34 +0100

    it works

--- ONNX Python/1-gen.py
+++ ONNX Python/1-gen.py
@@ -1,8 +1,8 @@
 import onnx
 from onnx import helper
 
-input_tensor = helper.make_tensor_value_info('input', onnx.TensorProto.INT32, [2, 2])
-constant_tensor = helper.make_tensor('constant', onnx.TensorProto.INT32, [2, 2], [1, 2, 3, 4])
+input_tensor = helper.make_tensor_value_info('input', onnx.TensorProto.INT8, [2, 2])
+constant_tensor = helper.make_tensor('constant', onnx.TensorProto.INT8, [2, 2], [1, 2, 3, 4])
 
 matmul_node = helper.make_node(
     'MatMul',
@@ -25,11 +25,26 @@ relu_node = helper.make_node(
     "relu-node"
 )
 
+matmul2_node = helper.make_node(
+    'MatMul',
+    ['relu-output', 'constant'],
+    ['matmul2-output'],
+    "matmul2-node"
+)
+
+bias2_node = helper.make_node(
+    "Add",
+    ["matmul2-output", "constant"],
+    ["bias2-output"],
+    "bias2-node"
+)
+
+
 graph_def = helper.make_graph(
-    [matmul_node, bias_node, relu_node],
+    [matmul_node, bias_node, relu_node, matmul2_node, bias2_node],
     'test-model',
     [input_tensor],
-    [helper.make_tensor_value_info('relu-output', onnx.TensorProto.INT32, [2, 2])],
+    [helper.make_tensor_value_info('bias2-output', onnx.TensorProto.INT8, [2, 2])],
     [constant_tensor]
 )
 
@@ -37,4 +52,4 @@ graph_def = helper.make_graph(
 model_def = helper.make_model(graph_def, producer_name='onnx-example')
 
 onnx.checker.check_model(model_def)
-onnx.save(model_def, "models/matmul-2x2-int32-with-scalar-and-bias-relu.onnx")
+onnx.save(model_def, "models/suite.onnx")
--- ONNX Python/3-load.py
+++ ONNX Python/3-load.py
@@ -3,6 +3,41 @@ from onnx import load
 with open("models/matmul-2x2-int32-with-scalar-and-bias-relu.onnx", "rb") as f:
     onnx_model = load(f)
 
-for n in onnx_model.graph.node:
-    print(n.output)
-    print("\n----\n")
+
+opset = {
+    "MatMul": 1,
+    "Add": 2,
+    "Relu": 3
+}
+
+layers = []
+layer = [onnx_model.graph.node[0]]
+
+for i in range(len(onnx_model.graph.node) - 1):
+    node = onnx_model.graph.node[i]
+    node_value = opset[node.op_type]
+
+    next_node = onnx_model.graph.node[i + 1]
+    next_node_value = opset[next_node.op_type]
+
+    #print(f"node: {node.name} = {node_value}")
+    #print(f"next_node: {next_node.name} = {next_node_value}")
+
+    if next_node_value > node_value:
+        # print("next node gets appended")
+        layer.append(next_node)
+    else:
+        #print("next node belongs to a new layer")
+        layers.append(layer)
+        layer = [next_node]
+
+    #print("-----------------")
+
+layers.append(layer)
+
+# print("\n\n\n\n")
+
+for layer in layers:
+    for node in layer:
+        print(node.name)
+    print("-------------")
