2b9a5432c13acb826e57a987b209909a56c55794
Andreas Lildballe <s214387@dtu.dk>
2024-02-08 13:47:08 +0100

    Update 3-load.py

--- ONNX Python/3-load.py
+++ ONNX Python/3-load.py
@@ -6,6 +6,32 @@ opset = {
     "Relu": 3
 }
 
+# https://onnx.ai/onnx/intro/concepts.html
+onnx_types = {
+    0: "UNDEFINED",
+    1: "FLOAT",
+    2: "UINT8",
+    3: "INT8",
+    4: "UINT16",
+    5: "INT16",
+    6: "INT32",
+    7: "INT64",
+    8: "STRING",
+    9: "BOOL",
+    10: "FLOAT16",
+    11: "DOUBLE",
+    12: "UINT32",
+    13: "UINT64",
+    14: "COMPLEX64",
+    15: "COMPLEX128",
+    16: "BFLOAT16",
+    17: "FLOAT8E4M3FN",
+    18: "FLOAT8E4M3FNUZ",
+    19: "FLOAT8E5M2",
+    20: "FLOAT8E5M2FNUZ",
+    21: "UINT4",
+    22: "INT4",
+}
 
 def sort_into_layers(onnx_model):
     layers = []
@@ -59,12 +85,16 @@ print(bias_location)
 initializer = onnx_model.graph.initializer
 weights = []
 biases = []
-# for each initializer, check if it is in weights_location or bias_location
+
+# for each initializer (containing the constant data), check if it is in weights_location or/and bias_location
 for init in initializer:
     if init.name in weights_location:
         weights.append(init)
     if init.name in bias_location:
         biases.append(init)
+
+# It seems that data is store din a field that depends on the type of the data
+# We need to check the type of the data and then access the correct field
 
 weight_matrices = []
 for w in weights:
