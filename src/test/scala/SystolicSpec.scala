93860c97f024706152196051e68cad3ffc6764ca
Andreas Lildballe <77052299+DreasL02@users.noreply.github.com>
2024-02-26 08:25:53 +0100

    Update SystolicSpec.scala

--- src/test/scala/SystolicSpec.scala
+++ src/test/scala/SystolicSpec.scala
@@ -10,16 +10,16 @@ import scala_utils.FixedPointConversion._
 
 class SystolicSpec extends AnyFreeSpec with ChiselScalatestTester {
   // ======= configure the test =======
-  val w = 8
+  val w = 7
   val wBig = 4 * w
-  val xDimension = 3
-  val yDimension = 4
-  val matrixCommonDimension = 2
-  val fixedPoint = 0
+  val numberOfColumns = 7 // number of columns in the result matrix
+  val numberOfRows = 5 // number of rows in the result matrix
+  val matrixCommonDimension = 10 // number of columns in the first matrix and number of rows in the second matrix
+  val fixedPoint = 3
   val signed = true
-  val numberOfTests = 1
+  val numberOfTests = 10
   val max = 3.2f
-  val min = 0.0f
+  val min = -3.2f
   val threshold = 0.1f
   val printing = Array.fill(numberOfTests)(false)
 
@@ -41,9 +41,9 @@ class SystolicSpec extends AnyFreeSpec with ChiselScalatestTester {
   for (testNum <- 0 until numberOfTests) {
     val enablePrinting = printing(testNum)
     "SystolicArray should calculate correctly for test %d".format(testNum) in {
-      test(new SystolicArray(w, wBig, xDimension, yDimension, signed)) { dut =>
-        var m1f = randomMatrix(yDimension, matrixCommonDimension, min, max, seeds(testNum * 2))
-        var m2f = randomMatrix(matrixCommonDimension, xDimension, min, max, seeds(testNum * 2 + 1))
+      test(new SystolicArray(w, wBig, numberOfRows, numberOfColumns, signed)) { dut =>
+        var m1f = randomMatrix(numberOfRows, matrixCommonDimension, min, max, seeds(testNum * 2))
+        var m2f = randomMatrix(matrixCommonDimension, numberOfColumns, min, max, seeds(testNum * 2 + 1))
 
         var mrf = calculateMatrixMultiplication(m1f, m2f)
         if (enablePrinting)
@@ -64,27 +64,38 @@ class SystolicSpec extends AnyFreeSpec with ChiselScalatestTester {
         mrf = calculateMatrixMultiplication(m1f, m2f)
         if (enablePrinting) {
           printMatrixMultiplication(m1f, m2f, mrf, "GOLDEN MODEL CALCULATION IN AFTER TRANSFORMATION BACK TO FLOATING")
+
+          println("-- Dimensions of matrices --")
+          println("a: (%d,%d)".format(m1.length, m1(0).length))
+          println("b: (%d,%d)".format(m2.length, m2(0).length))
         }
 
-        val mm1 = convertMatrixToMappedAMatrix(ms1, xDimension)
-        val mm2 = convertMatrixToMappedBMatrix(ms2, yDimension)
+
+        val mm1 = convertMatrixToMappedAMatrix(ms1, numberOfColumns + matrixCommonDimension - 2)
+        val mm2 = convertMatrixToMappedBMatrix(ms2, numberOfRows + matrixCommonDimension - 2)
 
         if (enablePrinting) {
           println("-- a --")
           print(matrixToString(mm1))
           println("-- b --")
           print(matrixToString(mm2))
-          println("----")
+          println("-- Dimensions --")
+          println("m_a: (%d,%d)".format(mm1.length, mm1(0).length))
+          println("m_b: (%d,%d)".format(mm2.length, mm2(0).length))
+          println("---------")
         }
 
         val max_number_of_cycles = mm1(0).length
+        if (enablePrinting) {
+          println("Max number of cycles: %d".format(max_number_of_cycles))
+        }
         for (cycle <- 0 until max_number_of_cycles) {
-          var values = "inp : "
+          var values = "a : "
           for (i <- mm1.indices) {
             values += mm1(i)(mm1(0).length - 1 - cycle).toString + ", "
             dut.io.a(i).poke(mm1(i)(mm1(0).length - 1 - cycle))
           }
-          values += "\nwei : "
+          values += "\nb : "
           for (i <- mm2(0).indices) {
             values += mm2(mm2.length - 1 - cycle)(i).toString + ", "
             dut.io.b(i).poke(mm2(mm2.length - 1 - cycle)(i))
