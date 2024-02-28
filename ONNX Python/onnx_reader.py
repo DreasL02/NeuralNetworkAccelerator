5a6c96cc9417f27b630ed55e211ae508532d8790
Ivan Hansgaard Hansen <ivanhhansen@gmail.com>
2024-02-28 13:15:05 +0100

    onnx graph decoding is working

--- ONNX Python/onnx_reader.py
+++ ONNX Python/onnx_reader.py
@@ -1,6 +1,77 @@
 from onnx import load
-with open("models/mnist-12-int8.onnx", "rb") as f:
+import pprint
+
+with open("models/sinus_float_model_epoch_1000.onnx", "rb") as f:
     onnx_model = load(f)
 
-with open("model.txt", "w") as f:
-    f.write(str(onnx_model))
+graph = {}
+
+def promote_dimensions(dim_array):
+    if len(dim_array) == 1:
+        return [1, dim_array[0]]
+
+    return dim_array
+
+def add_to_dict(dict, op_type, value):
+    if op_type not in dict:
+        dict[op_type] = []
+
+    dict[op_type].append(value)
+
+for input in onnx_model.graph.input:
+    graph[input.name] = {
+        "type": "input",
+        "name": input.name,
+        "data_type": input.type.tensor_type.elem_type,
+        "dims": [1, 1],
+        "output": input.name
+    }
+
+
+for initializer in onnx_model.graph.initializer:
+    graph[initializer.name] = {
+        "type": "initializer",
+        "name": initializer.name,
+        "data_type": initializer.data_type,
+        "dims": promote_dimensions(initializer.dims),
+        "raw_data": initializer.raw_data,
+        "output": initializer.name
+    }
+
+for node in onnx_model.graph.node:
+    graph[node.output[0]] = {
+        "type": "node",
+        "name": node.name,
+        "input": node.input,
+        "output": node.output,
+        "op_type": node.op_type
+    }
+
+
+def find_dimension(node_name):
+    if graph[node_name]["type"] in ["input", "initializer"]:
+        return graph[node_name]["dims"]
+
+    # we are dealing with a node (operation)
+    if graph[node_name]["op_type"] == "MatMul":
+        dim_x = find_dimension(graph[node_name]["input"][0])[0]
+        dim_y = find_dimension(graph[node_name]["input"][1])[1]
+        return [dim_x, dim_y]
+
+    return find_dimension(graph[node_name]["input"][0])
+
+
+for node in graph:
+    if graph[node]["type"] != "node":
+        # we are only interested in nodes (operations)
+        continue
+
+    pprint.pprint(graph[node])
+    print("")
+
+    for input in graph[node]["input"]:
+        pprint.pprint(graph[input])
+        print(find_dimension(input))
+        print()
+
+    print("\n\n\n")
