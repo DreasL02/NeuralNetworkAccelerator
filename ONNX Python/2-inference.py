917839aba908b5e8c674377dd01d816f28ecc1a7
Ivan Hansgaard Hansen <ivanhhansen@gmail.com>
2024-02-28 09:23:33 +0100

    onnx sinus model

--- ONNX Python/2-inference.py
+++ ONNX Python/2-inference.py
@@ -1,12 +1,27 @@
 import numpy
+import math
 import onnxruntime as rt
 
+model_name = "models/sinus_float_model_epoch_1000.onnx"
+
 sess = rt.InferenceSession(
-    "models/matmul-2x2-int32-with-scalar-and-bias-relu.onnx", providers=rt.get_available_providers())
+    model_name, providers=rt.get_available_providers())
 
-model_output = sess.run(None, {
-	"input": numpy.array([[-10, -20],
-				      [30, 40]], dtype=numpy.int32)
-})
 
-print(model_output)
+points = []
+for x in range(0, 64, 1):
+	x_val = x / 10
+
+	model_output = sess.run(None, {
+		"serving_default_dense_6_input:0": numpy.array([[x_val]], dtype=numpy.float32)
+	})
+	points.append((x_val, model_output[0][0][0]))
+
+#print(points)
+
+# plot points using matplotlib
+import matplotlib.pyplot as plt
+x, y = zip(*points)
+
+plt.plot(x, y)
+plt.savefig("sinus.png")
