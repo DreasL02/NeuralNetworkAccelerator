ea1497ec48045440860e20fd3b9162c1d2b36054
Andreas Lildballe <s214387@dtu.dk>
2024-03-06 13:54:44 +0100

    maybe rounder adding works

--- ONNX Python/onnx_reader.py
+++ ONNX Python/onnx_reader.py
@@ -15,12 +15,26 @@ graph = {}
 # 4. We should specify the connections between the nodes using the index of the nodes (operations) in the graph.
 # (5. We need to support at least up to 4D tensors)
 
+bit_width_multiplication = 8
+bit_width_base = bit_width_multiplication*4
+fixed_point_multiplication = 3
+fixed_point_base = fixed_point_multiplication*2
+
 
 def promote_dimensions(dim_array):
     if len(dim_array) == 1:
         return [1, dim_array[0]]
 
     return dim_array
+
+
+def demote_dimensions(dim_array):
+    print(type(dim_array))
+    if isinstance(dim_array, str):
+        print(dim_array)
+        return dim_array
+    print(dim_array[0])
+    return dim_array[0]
 
 
 def add_to_dict(dict, op_type, value):
@@ -30,14 +44,18 @@ def add_to_dict(dict, op_type, value):
     dict[op_type].append(value)
 
 
+index = 0
+
 for input in onnx_model.graph.input:
     graph[input.name] = {
         "type": "input",
         "name": input.name,
         "data_type": input.type.tensor_type.elem_type,
         "dims": [1, 1],
-        "output": input.name
+        "output": input.name,
+        "index": index
     }
+    index += 1
 
 
 for initializer in onnx_model.graph.initializer:
@@ -47,8 +65,10 @@ for initializer in onnx_model.graph.initializer:
         "data_type": initializer.data_type,
         "dims": promote_dimensions(initializer.dims),
         "raw_data": initializer.raw_data,
-        "output": initializer.name
+        "output": initializer.name,
+        "index": index
     }
+    index += 1
 
 for node in onnx_model.graph.node:
     graph[node.output[0]] = {
@@ -56,8 +76,63 @@ for node in onnx_model.graph.node:
         "name": node.name,
         "input": node.input,
         "output": node.output,
-        "op_type": node.op_type
+        "op_type": node.op_type,
+        "index": index
     }
+    index += 1
+
+# print the graph
+# pprint.pprint(graph)
+
+# Write bit width and fixed point for each node (operation) in the graph
+for node in graph:
+    if (graph[node]["type"] == "node" and graph[node]["op_type"] == "MatMul"):
+        graph[node]["bit_width_operands"] = bit_width_multiplication
+        graph[node]["bit_width_result"] = bit_width_base
+    else:
+        graph[node]["bit_width_operands"] = bit_width_base
+        graph[node]["bit_width_result"] = bit_width_base
+
+rounders = []
+for node in graph:
+    # if the bit width of the inputs and the result do not match, we need to introduce a rounder
+    if graph[node]["type"] != "node":
+        continue
+    bit_width_node = graph[node]["bit_width_operands"]
+    renamed_inputs = []
+    for input in graph[node]["input"]:
+        bit_width_input = graph[input]["bit_width_result"]
+
+        if bit_width_input != bit_width_node:
+            print("Introducing rounder for node: " + input)
+            name = "rounder_" + graph[input]["name"] + \
+                "_to_" + graph[node]["name"]
+            rounder_input = (graph[input]["output"])
+            rounder = {
+                "type": "node",
+                "name": name,
+                "bit_width_operands": bit_width_input,
+                "bit_width_result": bit_width_node,
+                "input": [demote_dimensions(rounder_input)],
+                "output": [name],
+                "op_type": "Rounder",
+                "index": index
+            }
+            index += 1
+            rounders.append(rounder)
+            graph[input]["output"] = rounder["input"]
+            renamed_inputs.append(rounder["output"][0])
+        else:
+            renamed_inputs.append(input)
+    graph[node]["input"] = renamed_inputs
+
+for rounder in rounders:
+    graph[rounder["name"]] = rounder
+
+
+# print the graph
+print("Graph:")
+pprint.pprint(graph)
 
 
 def find_dimension(node_name):
