eb19d9f8b1139fbfe33380876590340f89a3f793
Andreas Lildballe <s214387@dtu.dk>
2024-03-15 09:17:20 +0100

    Adder Tree finished

--- src/main/scala/AdderTree.scala
+++ src/main/scala/AdderTree.scala
@@ -2,42 +2,56 @@ import chisel3._
 import chisel3.util.{DecoupledIO, log2Ceil}
 import scala_utils.SmallModules._
 
+// Inspired by https://github.com/pConst/basic_verilog/blob/master/adder_tree.sv
+
 class AdderTree(
                  w: Int = 8,
-                 numberOfValues: Int = 4, // number of columns in the first matrix and number of rows in the second matrix
-                 enableDebuggingIO: Boolean = true
+                 numberOfInputs: Int = 4, // number of columns in the first matrix and number of rows in the second matrix
+
                ) extends Module {
 
   val io = IO(new Bundle {
-    val inputChannel = Flipped(new DecoupledIO(Vec(numberOfValues, UInt(w.W))))
+    val inputChannel = Flipped(new DecoupledIO(Vec(numberOfInputs, UInt(w.W))))
     val resultChannel = new DecoupledIO(UInt(w.W))
   })
 
-  // A adder tree is a tree of adders that sums all the values in the input channel
-  // The tree is built by adding pairs of values until there is only one value left
-  // The result is then valid and ready to be read
-  // Each stage of the tree is a register that holds the sum of the two values from the previous stage
+  val numberOfStages = log2Ceil(numberOfInputs) + 1
+  val numberOfInputsInt = math.pow(2, numberOfStages - 1).toInt
+  //println("numberOfStages: " + numberOfStages)
+  //println("numberOfInputsInt: " + numberOfInputsInt)
 
-  val numberOfLevels = log2Ceil(numberOfValues)
+  val data = Wire(Vec(numberOfStages, Vec(numberOfInputsInt, UInt(w.W))))
 
-  for (level <- 0 until numberOfLevels) {
-    val levelOutputs = Wire(Vec(numberOfValues / (1 << (level + 1)), UInt(w.W)))
-    if (level == 0) {
-      for (i <- 0 until levelOutputs.length by 2) {
-        levelOutputs(i / 2) := io.inputChannel.bits(i) + io.inputChannel.bits(i + 1)
+  for (stage <- 0 until numberOfStages) {
+    val numberOfOutputs = numberOfInputsInt >> stage
+    //println("numberOfOutputs: " + numberOfOutputs)
+
+    if (stage == 0) {
+      for (adder <- 0 until numberOfInputsInt) {
+        if (adder < numberOfInputs) {
+          data(stage)(adder) := io.inputChannel.bits(adder)
+          //println("Connected (stage, adder): (" + stage + ", " + adder + ")" + " to input channel " + adder)
+        } else {
+          data(stage)(adder) := 0.U
+          //println("Connected (stage, adder): (" + stage + ", " + adder + ")" + " to 0")
+        }
       }
     } else {
-      for (i <- 0 until levelOutputs.length by 2) {
-        levelOutputs(i / 2) := RegNext(levelOutputs(i) + levelOutputs(i + 1))
+      for (adder <- 0 until numberOfInputsInt) {
+        if (adder < numberOfOutputs) {
+          data(stage)(adder) := RegNext(data(stage - 1)(adder * 2) + data(stage - 1)(adder * 2 + 1))
+          //println("Connected (stage, adder): (" + stage + ", " + adder + ")" + " to previous stage " + (adder * 2) + " and " + (adder * 2 + 1))
+        } else {
+          data(stage)(adder) := 0.U
+          //println("Connected (stage, adder): (" + stage + ", " + adder + ")" + " to 0")
+        }
       }
-    }
-
-    if (level == numberOfLevels - 1) {
-      io.resultChannel.bits := levelOutputs(0)
     }
   }
 
-  val cyclesUntilOutputValid: Int = numberOfLevels // number of cycles until the adder tree is done and the result is valid
+  io.resultChannel.bits := data(numberOfStages - 1)(0)
+
+  val cyclesUntilOutputValid: Int = numberOfStages - 2 // number of cycles until the adder tree is done and the result is valid
 
   val readyToCompute = io.inputChannel.valid
 
--- src/test/scala/AdderTreeSpec.scala
+++ src/test/scala/AdderTreeSpec.scala
@@ -4,25 +4,42 @@ import org.scalatest.freespec.AnyFreeSpec
 
 class AdderTreeSpec extends AnyFreeSpec with ChiselScalatestTester {
   "AdderTree should sum all the values in the input channel" in {
-    test(new AdderTree(w = 8, numberOfValues = 4)) { c =>
-      c.io.inputChannel.valid.poke(true.B)
-      c.io.inputChannel.bits(0).poke(1.U)
-      c.io.inputChannel.bits(1).poke(2.U)
-      c.io.inputChannel.bits(2).poke(3.U)
-      c.io.inputChannel.bits(3).poke(4.U)
-      c.io.inputChannel.ready.expect(true.B)
-      c.io.resultChannel.valid.expect(false.B)
-      c.io.resultChannel.bits.expect(0.U)
-      c.clock.step(1)
-      c.io.resultChannel.valid.expect(false.B)
-      c.io.resultChannel.bits.expect(0.U)
-      c.clock.step(1)
-      c.io.resultChannel.valid.expect(false.B)
-      c.io.resultChannel.bits.expect(0.U)
-      c.clock.step(1)
-      c.io.resultChannel.valid.expect(true.B)
-      c.io.resultChannel.bits.expect(10.U)
+    test(new AdderTree(w = 8, numberOfInputs = 4)) { dut =>
+      dut.io.inputChannel.valid.poke(true.B)
+      dut.io.inputChannel.bits(0).poke(1.U)
+      dut.io.inputChannel.bits(1).poke(2.U)
+      dut.io.inputChannel.bits(2).poke(3.U)
+      dut.io.inputChannel.bits(3).poke(4.U)
+      dut.io.resultChannel.ready.poke(true.B)
+      dut.io.resultChannel.valid.expect(false.B)
+      dut.io.resultChannel.bits.expect(0.U)
+
+      while (!dut.io.resultChannel.valid.peek().litToBoolean) {
+        dut.clock.step()
+      }
+      dut.io.resultChannel.valid.expect(true.B)
+      dut.io.resultChannel.bits.expect(10.U)
     }
   }
+
+  "AdderTree should sum all the values in the input channel with 3 inputs" in {
+    test(new AdderTree(w = 8, numberOfInputs = 3)) { dut =>
+      dut.io.inputChannel.valid.poke(true.B)
+      dut.io.inputChannel.bits(0).poke(1.U)
+      dut.io.inputChannel.bits(1).poke(2.U)
+      dut.io.inputChannel.bits(2).poke(3.U)
+      dut.io.resultChannel.ready.poke(true.B)
+      dut.io.resultChannel.valid.expect(false.B)
+      dut.io.resultChannel.bits.expect(0.U)
+
+      while (!dut.io.resultChannel.valid.peek().litToBoolean) {
+        dut.clock.step()
+      }
+
+      dut.io.resultChannel.valid.expect(true.B)
+      dut.io.resultChannel.bits.expect(6.U)
+    }
+  }
+
 
 }
