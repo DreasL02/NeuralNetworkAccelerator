819a47db1d7dcef09eef6846799a6aebd7fe92e8
Andreas Lildballe <s214387@dtu.dk>
2024-03-29 16:11:21 +0100

    Update Conv4dSpec.scala

--- src/test/scala/Conv4dSpec.scala
+++ src/test/scala/Conv4dSpec.scala
@@ -298,4 +298,124 @@ class Conv4dSpec extends AnyFreeSpec with ChiselScalatestTester {
     }
   }
 
+  // 1 x 3 x 5 x 5
+  val test3Input = Array(
+    Array(
+      Array(
+        Array(1, 0, 1, 0, 2),
+        Array(1, 1, 3, 2, 1),
+        Array(1, 1, 0, 1, 1),
+        Array(2, 3, 2, 1, 3),
+        Array(0, 2, 0, 1, 0)
+      ),
+      Array(
+        Array(1, 0, 0, 1, 0),
+        Array(2, 0, 1, 2, 0),
+        Array(3, 1, 1, 3, 0),
+        Array(0, 3, 0, 3, 2),
+        Array(1, 0, 3, 2, 1)
+      ),
+      Array(
+        Array(2, 0, 1, 2, 1),
+        Array(3, 3, 1, 3, 2),
+        Array(2, 1, 1, 1, 0),
+        Array(3, 1, 3, 2, 0),
+        Array(1, 1, 2, 1, 1)
+      )
+    )
+  )
+
+  // 1 x 3 x 3 x 3
+  val test3Weights = Array(Array(
+    Array(
+      Array(0, 1, 0),
+      Array(0, 0, 2),
+      Array(0, 1, 0)
+    ),
+    Array(
+      Array(2, 1, 0),
+      Array(0, 0, 0),
+      Array(0, 3, 0)
+    ),
+    Array(
+      Array(1, 0, 0),
+      Array(1, 0, 0),
+      Array(0, 0, 2)
+    )
+  ))
+
+  "Convolution should calculate for 3D example" in {
+    test(new Conv4d(
+      w = 8,
+      wResult = 32,
+      inputDimensions = (test3Input.length, test3Input(0).length, test3Input(0)(0).length, test3Input(0)(0)(0).length),
+      kernelDimensions = (test3Weights.length, test3Weights(0).length, test3Weights(0)(0).length, test3Weights(0)(0)(0).length),
+      signed = true,
+      strides = (1, 1),
+      pads = (0, 0))) {
+      dut =>
+        val expectedOutput = Array(
+          Array(
+            Array(
+              Array(19, 13, 15),
+              Array(28, 16, 20),
+              Array(23, 18, 25)
+            )
+          )
+        )
+
+        dut.io.inputChannel.valid.poke(true.B)
+        dut.io.kernelChannel.valid.poke(true.B)
+
+        for (i <- test3Input.indices) {
+          for (j <- test3Input(i).indices) {
+            for (k <- test3Input(i)(j).indices) {
+              for (l <- test3Input(i)(j)(k).indices) {
+                dut.io.inputChannel.bits(i)(j)(k)(l).poke(test3Input(i)(j)(k)(l))
+              }
+            }
+          }
+        }
+
+        for (i <- test3Weights.indices) {
+          for (j <- test3Weights(i).indices) {
+            for (k <- test3Weights(i)(j).indices) {
+              for (l <- test3Weights(i)(j)(k).indices) {
+                dut.io.kernelChannel.bits(i)(j)(k)(l).poke(test3Weights(i)(j)(k)(l))
+              }
+            }
+          }
+        }
+
+        dut.io.outputChannel.ready.poke(true.B)
+
+        var cycle = 0
+
+        while (!dut.io.outputChannel.valid.peek().litToBoolean) {
+          dut.clock.step()
+          cycle += 1
+
+          if (toPrintNoPadding) {
+            println("Cycle: " + cycle)
+            for (i <- expectedOutput.indices) {
+              for (j <- expectedOutput(i).indices) {
+                for (k <- expectedOutput(i)(j).indices) {
+                  for (l <- expectedOutput(i)(j)(k).indices) {
+                    val result = dut.io.outputChannel.bits(i)(j)(k)(l).peek().litValue
+                    print("%d ".format(result))
+                  }
+                  println()
+                }
+                println()
+              }
+              println()
+            }
+          }
+
+          if (cycle > 100) {
+            fail("Timeout")
+          }
+        }
+    }
+  }
 }
