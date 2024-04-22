31be256ebc84b94aeb706305d26896cc3c5d09ed
Ivan Hansgaard Hansen <ivanhhansen@gmail.com>
2024-04-22 12:09:02 +0200

    uart output

--- src/main/scala/stages/OutputStage.scala
+++ src/main/scala/stages/OutputStage.scala
@@ -2,6 +2,9 @@ package stages
 
 import onnx.Operators.OutputType
 import chisel3._
+import communication.chisel.lib.uart.BufferedUartRxForTestingOnly
+import module_utils.ByteIntoFlatVectorCollector
+import operators.Reshape
 
 class OutputStage(
                    wIn: Int,
@@ -18,14 +21,34 @@ class OutputStage(
   override lazy val shapeOut = outputShape
 
   if (implementation == OutputImplementation.Uart) {
-    // TODO: Implement UART output, for now raise an error
     assert(outputShape == (1, 1, 1, 1), "UART output only supports one output")
     assert(wOut == 1, "UART output only supports one bit-width output")
 
-    throw new NotImplementedError("UART output not implemented")
+    /*
+    val totalElements = outputShape._1 * outputShape._2 * outputShape._3 * outputShape._4
+    val inputBitWidth = totalElements * wOut
+
+    val bytesRequired = (inputBitWidth / 8.0f).ceil.toInt
+    val bufferedUartRx = Module(new BufferedUartRxForTestingOnly(frequency, baudRate, bytesRequired))
+
+    bufferedUartRx.io.rxd := io.inputChannel.bits(0)(0)(0)(0)
+    io.inputChannel.ready := true.B
+    bufferedUartRx.io.outputChannel.ready := io.outputChannel.ready
+
+    // Convert the bytes into a flat vector of correct width numbers
+    val byteConverter = Module(new ByteIntoFlatVectorCollector(bytesRequired, wOut))
+
+    byteConverter.io.inputChannel <> bufferedUartRx.io.outputChannel
+
+    val reshaper = Module(new Reshape(wOut, (1, 1, 1, bytesRequired), (1, 1, 1, 1), outputShape))
+
+    reshaper.io.inputChannel <> byteConverter.io.outputChannel
+
+    io.outputChannel <> reshaper.io.outputChannel
 
     io.outputChannel.ready := true.B
 
+     */
     latency = 0
     dspUsage = 0
   } else {
