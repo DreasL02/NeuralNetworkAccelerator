edfb8f0b2f7548341bec29853c11c36a85398ad0
Andreas Lildballe <77052299+DreasL02@users.noreply.github.com>
2024-05-06 14:01:44 +0200

    max poool

--- src/test/scala/MaxPoolSpec.scala
+++ src/test/scala/MaxPoolSpec.scala
@@ -3,6 +3,7 @@ import org.scalatest.freespec.AnyFreeSpec
 import chisel3._
 import chiseltest._
 import operators.MaxPool
+import scala_utils.FixedPointConversion.convertFloatMatrixToFixedMatrix
 
 
 // Test cases from:
@@ -22,7 +23,7 @@ class MaxPoolSpec extends AnyFreeSpec with ChiselScalatestTester {
       Array(4, 6)
     )
 
-    test(new MaxPool(8, matrix.length, matrix(0).length, (2, 2), (0, 0), (2, 2), true)) { dut =>
+    test(new MaxPool(8, matrix.length, matrix(0).length, (2, 2), (0, 0), (2, 2), true, toPrint)) { dut =>
       for (i <- 0 until matrix.length) {
         for (j <- 0 until matrix(i).length) {
           dut.io.inputChannel.bits(i)(j).poke(matrix(i)(j).U)
@@ -69,7 +70,7 @@ class MaxPoolSpec extends AnyFreeSpec with ChiselScalatestTester {
       Array(6, 6),
     )
 
-    test(new MaxPool(8, matrix.length, matrix(0).length, (3, 3), (0, 0), (1, 1), true)) { dut =>
+    test(new MaxPool(8, matrix.length, matrix(0).length, (3, 3), (0, 0), (1, 1), true, toPrint)) { dut =>
       for (i <- 0 until matrix.length) {
         for (j <- 0 until matrix(i).length) {
           dut.io.inputChannel.bits(i)(j).poke(matrix(i)(j).U)
@@ -115,7 +116,7 @@ class MaxPoolSpec extends AnyFreeSpec with ChiselScalatestTester {
       Array(5),
     )
 
-    test(new MaxPool(8, matrix.length, matrix(0).length, (3, 3), (0, 0), (2, 2), true)) { dut =>
+    test(new MaxPool(8, matrix.length, matrix(0).length, (3, 3), (0, 0), (2, 2), true, toPrint)) { dut =>
       for (i <- 0 until matrix.length) {
         for (j <- 0 until matrix(i).length) {
           dut.io.inputChannel.bits(i)(j).poke(matrix(i)(j).U)
@@ -147,6 +148,57 @@ class MaxPoolSpec extends AnyFreeSpec with ChiselScalatestTester {
         }
       }
     }
+  }
+
+  "MaxPool should behave correcly in all negative matrix" in {
+    val matrix = Array(
+      Array(-2, -3, -4, 0),
+      Array(-1, -5, -3, -2),
+      Array(0, -4, -2, -3),
+      Array(-1, 0, -6, -1)
+    )
+
+    val expected = Array(
+      Array(-1, 0),
+      Array(0, -1)
+    )
+
+    val matrixUnsigned = convertFloatMatrixToFixedMatrix(matrix.map(_.map(_.toFloat)), 0, 8, true)
+    val expectedUnsigned = convertFloatMatrixToFixedMatrix(expected.map(_.map(_.toFloat)), 0, 8, true)
+
+    test(new MaxPool(8, matrix.length, matrix(0).length, (2, 2), (0, 0), (2, 2), true, toPrint)) { dut =>
+      for (i <- 0 until matrix.length) {
+        for (j <- 0 until matrix(i).length) {
+          dut.io.inputChannel.bits(i)(j).poke(matrixUnsigned(i)(j).U)
+        }
+      }
+      dut.io.inputChannel.valid.poke(true.B)
+      dut.io.outputChannel.ready.poke(true.B)
+
+      while (!dut.io.outputChannel.valid.peek().litToBoolean) {
+        dut.clock.step()
+      }
+
+      val result = dut.io.outputChannel.bits
+
+      if (toPrint) {
+        for (i <- 0 until result.length) {
+          for (j <- 0 until result(i).length) {
+            print(result(i)(j).peek().litValue)
+            print(" ")
+          }
+          println()
+        }
+        println()
+      }
+
+      for (i <- 0 until result.length) {
+        for (j <- 0 until result(i).length) {
+          result(i)(j).expect(expectedUnsigned(i)(j).U)
+        }
+      }
+    }
+
   }
 }
 
